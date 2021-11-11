87dfcda3ad522264c57da480b378aa7c82da09f0
Maksymilian Krajewski <maksymilian.krajewski@yahoo.com>
2021-11-11 10:52:17 +0100

    add files

---
+++ src/components/List/List.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import styles from './List.scss';
+import Hero from '../Hero/Hero';
+import PropTypes from 'prop-types';
+import Column from '../Column/ColumnContainer.js';
+import {settings} from '../../data/dataStore';
+import ReactHtmlParser from 'react-html-parser';
+import Creator from '../Creator/Creator';
+import Container from '../Container/Container';
+
+class List extends React.Component {
+  static propTypes = {
+    title: PropTypes.node.isRequired,
+    description: PropTypes.node,
+    columns: PropTypes.array,
+    image: PropTypes.string,
+    addColumn: PropTypes.func,
+  }
+
+  static defaultProps = {
+    description: ReactHtmlParser(settings.defaultListDescription),
+  }
+
+  render() {
+    const {title, image, description, columns, addColumn} = this.props;
+
+    return (
+      <Container>
+        <section className={styles.component}>
+            <Hero titleText={title} image={image} />
+          <div className={styles.description}>
+            {description}
+          </div>
+          <div className={styles.columns}>
+            {columns.map(columnData => (
+              <Column key={columnData.id} {...columnData} />
+            ))}
+          </div>
+          <div className={styles.creator}>
+            <Creator text={settings.columnCreatorText} action={addColumn}/>
+          </div>
+        </section>
+      </Container>
+    );
+  }
+}
+
+export default List;
